d93ce0a6d8c2837659687b8e57e815e5679b01b5
heribertofilho <heriberto.filho@outlook.com>
2017-03-23 17:36:43 -0300

    Correção do Adapter e da MainActivity.

--- app/src/main/java/com/example/xaid/smartfarm/MainActivity.java
+++ app/src/main/java/com/example/xaid/smartfarm/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.xaid.smartfarm;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
@@ -9,12 +9,9 @@ import com.example.xaid.smartfarm.Adapter.SensoresAdapter;
 import com.example.xaid.smartfarm.Models.Sensor;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
-    private SensoresAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
@@ -24,22 +21,17 @@ public class MainActivity extends AppCompatActivity {
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
         mRecyclerView.setHasFixedSize(true);
 
-        // use a linear layout manager
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        // specify an adapter (see also next example)
-
-
-        ArrayList<Sensor> myDataset = new ArrayList<Sensor>();
+        ArrayList<Sensor> myDataset = new ArrayList<>();
 
         Sensor s1 = new Sensor(1, 0, false, 0);
         myDataset.add(s1);
-        mAdapter = new SensoresAdapter(this, myDataset);
-        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setAdapter(new SensoresAdapter(this, myDataset));
+        RecyclerView.LayoutManager layout = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(layout);
     }
 }
